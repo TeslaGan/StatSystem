34606b82de3c88f355ac274707652e6ba09b36b5
TeslaGan <139454272+TeslaGan@users.noreply.github.com>
2026-09-02 20:32:45 +0300

    Add stat component snapshot

---
+++ StatComponentSnapshot.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+
+namespace StatSystem
+{
+    public sealed class StatComponentSnapshot<TStat, TComponent>
+    {
+        public TComponent Component { get; }
+        public float Value { get; }
+        public IReadOnlyList<StatSourceSnapshot<TStat, TComponent>> Sources { get; }
+
+        internal StatComponentSnapshot(
+            TComponent component,
+            float value,
+            IReadOnlyList<StatSourceSnapshot<TStat, TComponent>> sources)
+        {
+            Component = component;
+            Value = value;
+            Sources = sources;
+        }
+    }
+}
