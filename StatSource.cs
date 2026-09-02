c3976116bde3683d1dcc5bf40709b210e461b971
TeslaGan <139454272+TeslaGan@users.noreply.github.com>
2026-09-02 20:32:26 +0300

    Add base stat source

---
+++ StatSource.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace StatSystem
+{
+    public abstract class StatSource<TStat, TComponent> : IStatSource<TStat, TComponent>
+    {
+        public event Action Invalidated;
+
+        public abstract TStat Stat { get; }
+        public abstract TComponent Component { get; }
+        public abstract StatModifierKind Kind { get; }
+        public abstract float Value { get; }
+
+        protected void Invalidate()
+        {
+            Invalidated?.Invoke();
+        }
+    }
+}
