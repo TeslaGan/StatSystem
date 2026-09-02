34069ab1419211c208c7f0ee272e24fd4625c9e4
TeslaGan <139454272+TeslaGan@users.noreply.github.com>
2026-09-02 20:32:32 +0300

    Add reference comparer

---
+++ ReferenceEqualityComparer.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace StatSystem
+{
+    internal sealed class ReferenceEqualityComparer<T> : IEqualityComparer<T>
+        where T : class
+    {
+        public static ReferenceEqualityComparer<T> Instance { get; } = new();
+
+        private ReferenceEqualityComparer()
+        {
+        }
+
+        public bool Equals(T x, T y)
+        {
+            return ReferenceEquals(x, y);
+        }
+
+        public int GetHashCode(T obj)
+        {
+            return RuntimeHelpers.GetHashCode(obj);
+        }
+    }
+}
