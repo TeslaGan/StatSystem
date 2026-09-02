fe76cbfc8f3806955f94748a9bdc5878a6ddbef6
TeslaGan <139454272+TeslaGan@users.noreply.github.com>
2026-09-02 20:32:38 +0300

    Add stat source snapshot

---
+++ StatSourceSnapshot.cs
@@ -0,0 +1,22 @@
+namespace StatSystem
+{
+    public readonly struct StatSourceSnapshot<TStat, TComponent>
+    {
+        public IStatSource<TStat, TComponent> Source { get; }
+        public TStat Stat { get; }
+        public TComponent Component { get; }
+        public StatModifierKind Kind { get; }
+        public float Value { get; }
+
+        internal StatSourceSnapshot(
+            IStatSource<TStat, TComponent> source,
+            float value)
+        {
+            Source = source;
+            Stat = source.Stat;
+            Component = source.Component;
+            Kind = source.Kind;
+            Value = value;
+        }
+    }
+}
